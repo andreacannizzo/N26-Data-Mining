b50e64343912803a81fceffc8298a7110827f190
occhiodelloshinigami <cannizzo1@hotmail.com>
2021-10-10 19:59:22 +0200

    login, scroll to bottom done

--- definitions.py
+++ definitions.py
@@ -1,4 +1,32 @@
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-def log_in(browser):
-    print("ciao")
+def login(browser, username_str, password_str):
+    username = browser.find_element_by_name('username')
+    username.clear()
+    username.send_keys(username_str)
+    password = browser.find_element_by_name('password')
+    password.clear()
+    password.send_keys(password_str)
+    submit = browser.find_element_by_xpath("//button")
+    submit.submit()
+
+
+def scroll_to_bottom(browser):
+    try:
+        button = WebDriverWait(browser, 10).until(
+            EC.visibility_of_element_located((By.XPATH, "//*[@title='Successivo']")))
+        while button.size != 0:
+            try:
+                button = WebDriverWait(browser, 10).until(
+                    EC.visibility_of_element_located((By.XPATH, "//*[@title='Successivo']")))
+            except:
+                print("Sono arrivato alla fine")
+            button.send_keys(Keys.RETURN)
+            browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    except:
+        print("Sono già arrivato alla fine")
+
--- main.py
+++ main.py
@@ -1,23 +1,24 @@
-import asyncio
-from time import sleep
+from inputs import *
+from definitions import *
+import chromedriver_autoinstaller
+from selenium import webdriver
 
-from pyppeteer import launch
 
+auto_chromedriver = chromedriver_autoinstaller.install()
+browser = webdriver.Chrome(auto_chromedriver)
+browser.get('https://app.n26.com/login')
+login(browser, username, password)
+scroll_to_bottom(browser)
+# "//li/" for web elements, "//li/div/p/span/span[1]/a" to obtain href of each element
+# but the element whit the href is not clickable
+URL_elements = browser.find_elements_by_xpath("//li/div/p/span/span[1]/a")
 
-async def main():
-    browser = await launch(
-        headless=False,
-        handleSIGINT=False,
-        handleSIGTERM=False,
-        handleSIGHUP=False,
-    )
-    sleep(3)
-    page = await browser.newPage()
-    sleep(3)
-    await page.goto('https://app.n26.com/login')
-    sleep(3)
-    
-    sleep(600)
-    # await browser.close()
+# elemento freccia in basso espandi pagina
+# from selenium.webdriver.common.keys import Keys
+# button = browser.find_element_by_xpath("//*[@title='Successivo' and @type='button']")
+# button.send_keys(Keys.RETURN)
 
-asyncio.get_event_loop().run_until_complete(main())
+# per creare un elemento ogni transazione visibile nella pagina
+# browser.find_elements_by_xpath("//li/div/p/span/span[1]/a")
+
+browser.close()
